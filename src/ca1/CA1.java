af453b45039828f040aba3ac277fa72389e514f5
Teresa Ventaja <71657946+teresaventaja@users.noreply.github.com>
2024-01-15 19:02:38 +0000

    1

--- src/ca1/CA1.java
+++ src/ca1/CA1.java
@@ -11,7 +11,23 @@ package ca1;
 public class CA1 {
 
     /**
-     * @param args the command line arguments
+     * When in operation, the program will be given a file named “students.txt” – this contains the details of (fictitious) students in the following format: 
+
+Line 1 – First Name> second Name>
+Line 2 – Number of classes>
+Line 3 – Student number>
+
+Your task is to: 
+1)	Read the data from the file and check that it is valid. The file may contain more than one student, so an appropriate loop should be used. The data must obey the following rules: 
+a)	the first name must be letters only; 
+b)	The second name can be letters and/or numbers and must be separated from the first name by a single space; 
+c)	the number of classes must be an integer value between 1 and 8 (inclusive), and 
+d)	the student “number” must be a minimum of 6 characters with the first 2 characters being numbers, the 3rd  and 4th characters (and possibly 5th ) being a letter, and everything after the last letter character being a number.
+2)	If the data is not valid, you should output a useful error message on screen to the user.
+3)	If the data is valid, then you have to output the data to a file named status.txt, in the following format:
+Student number> - Second Name>
+Workload>
+
      */
     public static void main(String[] args) {
         // TODO code application logic here
