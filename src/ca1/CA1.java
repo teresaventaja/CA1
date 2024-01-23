dcad63494d4594a2ffcf3822abb0cf5d986a90cc
Teresa Ventaja <71657946+teresaventaja@users.noreply.github.com>
2024-01-23 16:05:36 +0000

    validation does not print errors

--- src/ca1/CA1.java
+++ src/ca1/CA1.java
@@ -22,16 +22,8 @@ public class CA1 {
      * Github: https://github.com/teresaventaja/CA1
      * When in operation, the program will be given a file named “students.txt” – this contains the details of (fictitious) students in the following format: 
 
-Line 1 – First Name> second Name>
-Line 2 – Number of classes>
-Line 3 – Student number>
-
 Your task is to: 
 1)	Read the data from the file and check that it is valid. The file may contain more than one student, so an appropriate loop should be used. The data must obey the following rules: 
-a)	the first name must be letters only; 
-b)	The second name can be letters and/or numbers and must be separated from the first name by a single space; 
-c)	the number of classes must be an integer value between 1 and 8 (inclusive), and 
-d)	the student “number” must be a minimum of 6 characters with the first 2 characters being numbers, the 3rd  and 4th characters (and possibly 5th ) being a letter, and everything after the last letter character being a number.
 2)	If the data is not valid, you should output a useful error message on screen to the user.
 3)	If the data is valid, then you have to output the data to a file named status.txt, in the following format:
 Student number> - Second Name>
@@ -40,17 +32,18 @@ Workload>
      */
     public static void main(String[] args) throws FileNotFoundException {
         // TODO code application logic here
- 
-        assignVariables firstAttempt = new assignVariables();        
+    //String name = null; 
+   // String numClasses = null; 
+   // String studentNumber = null; 
+        assignVariables firstAttempt = new assignVariables();  
         firstAttempt.assignStudents("students.txt");
+       // firstAttempt.dataValidation(name, numClasses, studentNumber);
         
-        /*
-        ArrayList<Students> AllStudents = new ArrayList<>();
-        assignVariables firstAttempt = new assignVariables();
-        firstAttempt.findStudents("students.txt");
+       // firstAttempt.dataValidation();
+                
+       
         
-        
-        */
+
 
         
 
--- src/ca1/assignVariables.java
+++ src/ca1/assignVariables.java
@@ -21,8 +21,10 @@ import java.util.Scanner;
  */
 public class assignVariables {
 
-       List<Students> studentList = new ArrayList<>();
-      public String name; 
+    List<Students> studentList = new ArrayList<>();
+    public String name; 
+    public String numClasses; 
+    public String studentNumber; 
     
     
 public String[][] assignStudents(String file) {
@@ -36,27 +38,34 @@ try {
         while ((name = br.readLine()) != null) { //reads first line
             //name = br.readLine();
             name = name.trim(); // Remove whitespaces
-            String numClasses = br.readLine();  //reads second line
+            numClasses = br.readLine();  //reads second line
             if (numClasses == null) { 
             System.out.println("Incomplete data after student name: " + name);
             break;
                 }
-            String studentNumber = br.readLine(); // Read third line (student number)
+            studentNumber = br.readLine(); // Read third line (student number)
             if (studentNumber == null) {
             System.out.println("Incomplete data after number of classes for student: " + name);
             break;
             } 
-                    
-
-        // A constructor that takes student name, number of classes, and student number
-   
-               Students newStudent = new Students(name, numClasses, studentNumber);
-                studentList.add(newStudent);
                 
-                
-                   //Students[] studentsArray = new Students[studentList.size()];
-   //Students[] studentsArray2 = studentList.toArray(studentsArray);
+            
+            
+            
+            List<String> validationErrors = dataValidation(name, numClasses, studentNumber);
+        if (validationErrors.isEmpty()) {
+            Students newStudent = new Students(name, numClasses, studentNumber);
+            studentList.add(newStudent);
+        } else {
+            for (String error : validationErrors) {
+                System.out.println(error); // Print each error message
+            }
+            continue; // Skip to the next iteration since this student failed validation
+        }
+ 
         
+                       Students newStudent = new Students(name, numClasses, studentNumber);
+                studentList.add(newStudent);
          String[][] studentDataArray = new String[studentList.size()][3];
                 
                  for (int i = 0; i < studentList.size(); i++) {
@@ -76,44 +85,83 @@ try {
             System.out.println(); // Print an empty line for better readability
         }
         
-                
+
+
+
                  }
+
             br.close();
+  
         } catch (IOException e) {
             System.out.println(e);
             e.printStackTrace();
         } return null;
-} 
-      
-            /*
-   Students[] studentsArray = new Students[studentList.size()];
-   Students[] studentsArray2 = studentList.toArray(studentsArray);
         
-         String[][] studentDataArray = new String[studentList.size()][3];
+}
+        
+public List<String> dataValidation(String name, String numClasses, String studentNumber) {
+    List<String> errors = new ArrayList<>();
+    //boolean isValid = true;
+int indexOfSpace = name.indexOf(" ");
+    if (indexOfSpace == -1) {
+        errors.add("Invalid format: name does not contain a space");
+        
+    //System.out.println("Invalid format: name does not contain a space");
+            //continue; 
+            }
+            String firstName =  name.substring(0,indexOfSpace);
+            String secondName =  name.substring(indexOfSpace+1);
+    if (!firstName.matches("[A-Za-z]+")) {
+                errors.add("The first name must contain only letters");        
+       // System.out.println("The first name must contain only letters");
+            } else if (!secondName.matches("^[a-zA-Z0-9]+$")) {
+                errors.add("The second name must contain letters and / or numbers");  
+                //System.out.println("The second name must contain letters and / or numbers");
+            }
+    if (!numClasses.matches("[0-9]+")) {
+        errors.add("The number of classes must contain numbers");          
+        //System.out.println("The number of classes must contain numbers");
+            }
+    studentNumber = studentNumber.trim(); // Remove whitespaces
+    String firstPartStudentNumber = studentNumber.substring(0,1);
+    String secondPartSN1 = studentNumber.length() > 2 ? studentNumber.substring(2, 3) : ""; //studentNumber.substring(2,3);
+    String secondPartSN2 = studentNumber.length() > 2 ? studentNumber.substring(2, 4) : ""; //studentNumber.substring(2,4);
+    String lastPartTrigger = studentNumber.length() > 4 ? studentNumber.substring(4, 5) : "";//studentNumber.substring(4,4);
+    String lastPartSN;
+    if (studentNumber.length() <= 6) {
+        errors.add("The student number must be at least 6 characters long");
+                //System.out.println("The student number must be at least 6 characters long");
+            } else if (!firstPartStudentNumber.matches("[0-9]+")) {
+               // System.out.println("The student number must start with 2 numbers");
+                errors.add("The student number must start with 2 numbers");
+            } else if ((!secondPartSN1.matches("[A-Za-z]+")) || (!secondPartSN2.matches("[A-Za-z]+"))) {
+               errors.add("The 3rd, 4rd - and sometimes the 5th character too - of the student number must be letters");
+               // System.out.println("The 3rd, 4rd - and sometimes the 5th character too - of the student number must be letters");
+    if (lastPartTrigger.matches("[A-Za-z]+")) {
+            lastPartSN = studentNumber.length() > 5 ? studentNumber.substring(5) : ""; //lastPartSN = studentNumber.substring(5);
+                if (!lastPartSN.matches("[0-9]+")){
+                     errors.add("The last part of the student number must be numbers");
+   // System.out.println("The last part of the student number must be numbers");
+    }
+            } else if (lastPartTrigger.matches("[0-9]+")) {
+                lastPartSN = studentNumber.length() > 4 ? studentNumber.substring(4) : ""; //   lastPartSN = studentNumber.substring(4);
+                if (!lastPartSN.matches("[0-9]+")){
+                    errors.add("The last part of the student number must be numbers");
+    //System.out.println("The last part of the student number must be numbers");
+    }
+            }
+
+}
+        // Return true if data is valid, false otherwise
+ // or false based on validation
+    return errors; 
+}
+        
+ 
+
+}
         
 
-        for (int i = 0; i < studentList.size(); i++) {
-            //assignVariables student = studentList.get(i);
-            Students student = studentList.get(i);
-            studentDataArray[i][0] = student.getStudentName();
-            studentDataArray[i][1] = student.getNumberOfClasses();
-            studentDataArray[i][2] = student.getStudentNumber();
-        }
-        
-      //  return studentDataArray;
- // checking if it worked
-        for (Students student : studentsArray2) {
-            System.out.println("Student Name: " + student.getStudentName());
-            System.out.println("Number of Classes: " + student.getNumberOfClasses());
-            System.out.println("Student Number: " + student.getStudentNumber());
-            System.out.println(); // Print an empty line for better readability
-        }
-*/
-            
-  // checking if it worked
-  
-        
-} 
 
         
 
