f8296a09eb1cc5742cdda1e248c3520a2a90d817
Teresa Ventaja <71657946+teresaventaja@users.noreply.github.com>
2024-01-23 14:59:22 +0000

    working code

--- src/ca1/assignVariables.java
+++ src/ca1/assignVariables.java
@@ -20,11 +20,16 @@ import java.util.Scanner;
  * @author User
  */
 public class assignVariables {
-   List<Students> studentList = new ArrayList<>();
-public String name;        
 
+       List<Students> studentList = new ArrayList<>();
+      
+    
+    
 public String[][] assignStudents(String file) {
+
+ 
 try {
+           String name; 
             //File file = new File("students.txt");
             //FileReader fr = new FileReader(file);
             BufferedReader br = new BufferedReader(new FileReader (file));
@@ -40,187 +45,77 @@ try {
             if (studentNumber == null) {
             System.out.println("Incomplete data after number of classes for student: " + name);
             break;
-            }
-           
- 
-            int indexOfSpace = name.indexOf(" ");
-    if (indexOfSpace == -1) {
-            System.out.println("Invalid format: name does not contain a space");
-            continue; 
-            }
-            String firstName =  name.substring(0,indexOfSpace);
-            String secondName =  name.substring(indexOfSpace+1);
-    if (!firstName.matches("[A-Za-z]+")) {
-                System.out.println("The first name must contain only letters");
-            } else if (!secondName.matches("^[a-zA-Z0-9]+$")) {
-                System.out.println("The second name must contain letters and / or numbers");
-            }
-    if (!numClasses.matches("[0-9]+")) {
-                System.out.println("The number of classes must contain numbers");
-            }
-    studentNumber = studentNumber.trim(); // Remove whitespaces
-    String firstPartStudentNumber = studentNumber.substring(0,1);
-    String secondPartSN1 = studentNumber.substring(2,3);
-    String secondPartSN2 = studentNumber.substring(2,4);
-    String lastPartTrigger = studentNumber.substring(4,4);
-    String lastPartSN;
-    if (studentNumber.length() <= 6) {
-                System.out.println("The student number must be at least 6 characters long");
-            } else if (!firstPartStudentNumber.matches("[0-9]+")) {
-                System.out.println("The student number must start with 2 numbers");
-            } else if ((!secondPartSN1.matches("[A-Za-z]+")) || (!secondPartSN2.matches("[A-Za-z]+"))) {
-                System.out.println("The 3rd, 4rd - and sometimes the 5th character too - of the student number must be letters");
-    if (lastPartTrigger.matches("[A-Za-z]+")) {
-            lastPartSN = studentNumber.substring(5);
-                if (!lastPartSN.matches("[0-9]+")){
-    System.out.println("The last part of the student number must be numbers");
-    }
-            } else if (lastPartTrigger.matches("[0-9]+")) {
-            lastPartSN = studentNumber.substring(4);
-                if (!lastPartSN.matches("[0-9]+")){
-    System.out.println("The last part of the student number must be numbers");
-    }
-            }
-br.close();
+            } 
                     
 
-
-// A constructor that takes student name, number of classes, and student number
+        // A constructor that takes student name, number of classes, and student number
    
-                Students student = new Students(name, numClasses, studentNumber);
-                studentList.add(student);
-            }
-            
-        }
-            
-        Students[] studentsArray = new Students[studentList.size()];
-        studentsArray = studentList.toArray(studentsArray);
+               Students newStudent = new Students(name, numClasses, studentNumber);
+                studentList.add(newStudent);
+                
+                
+                   Students[] studentsArray = new Students[studentList.size()];
+   Students[] studentsArray2 = studentList.toArray(studentsArray);
         
-        
-        String[][] studentDataArray = new String[studentList.size()][3];
-        for (int i = 0; i < studentList.size(); i++) {
-            Students student = studentList.get(i);
-            studentDataArray[i][0] = student.getStudentName();
-            studentDataArray[i][1] = student.getNumberOfClasses();
-            studentDataArray[i][2] = student.getStudentNumber();
+         String[][] studentDataArray = new String[studentList.size()][3];
+                
+                 for (int i = 0; i < studentList.size(); i++) {
+            //assignVariables student = studentList.get(i);
+            newStudent = studentList.get(i);
+            studentDataArray[i][0] = newStudent.getStudentName();
+            studentDataArray[i][1] = newStudent.getNumberOfClasses();
+            studentDataArray[i][2] = newStudent.getStudentNumber();
         }
+        
+      //  return studentDataArray;
  // checking if it worked
-        for (Students student : studentsArray) {
+        for (Students student : studentList) {
             System.out.println("Student Name: " + student.getStudentName());
             System.out.println("Number of Classes: " + student.getNumberOfClasses());
             System.out.println("Student Number: " + student.getStudentNumber());
             System.out.println(); // Print an empty line for better readability
         }
-          
+        
+                
+                 }
+            br.close();
         } catch (IOException e) {
             System.out.println(e);
+            e.printStackTrace();
         } return null;
-       
+} 
+      
+            /*
+   Students[] studentsArray = new Students[studentList.size()];
+   Students[] studentsArray2 = studentList.toArray(studentsArray);
         
+         String[][] studentDataArray = new String[studentList.size()][3];
+        
+
+        for (int i = 0; i < studentList.size(); i++) {
+            //assignVariables student = studentList.get(i);
+            Students student = studentList.get(i);
+            studentDataArray[i][0] = student.getStudentName();
+            studentDataArray[i][1] = student.getNumberOfClasses();
+            studentDataArray[i][2] = student.getStudentNumber();
+        }
+        
+      //  return studentDataArray;
+ // checking if it worked
+        for (Students student : studentsArray2) {
+            System.out.println("Student Name: " + student.getStudentName());
+            System.out.println("Number of Classes: " + student.getNumberOfClasses());
+            System.out.println("Student Number: " + student.getStudentNumber());
+            System.out.println(); // Print an empty line for better readability
+        }
+*/
+            
+  // checking if it worked
+  
         
 } 
 
         
 
-}
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-   /* 
-  
-ArrayList<Students> AllStudents = new ArrayList<>();
-  
-public String name;
-public String numClasses;
-public String studentNumber;
-public String line = ""; 
-public int length = 0;
-
-public int findLength (String fileName) throws FileNotFoundException, IOException {
-     try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
-          while (reader.readLine() != null) length++;
-          System.out.println(length);
-      } catch (IOException e) {
-          e.printStackTrace();
-      }
-      return length;
-
-  }
-
-
-public String[][] all_students = new String[length][3];
-        
-public String[][] assignStudents() {
-       for (int row=0; row<all_students.length; row++) {
-            System.out.println(all_students[row]);
-            for (int col=0; col<all_students[row].length; col++) {
-                System.out.println(all_students[row][col]);
-                System.out.println(all_students[0][2]);
-            }
-        }
-    
-return all_students;
-}
-
-
-public void findStudents (String fileName) throws FileNotFoundException {
-Scanner scanner = new Scanner(new File(fileName));
 
     
-int line = 1;
-while (scanner.hasNextLine()) {
-    if (line % 3 == 1) {
-        name = scanner.nextLine();
-    } else if (line % 3 == 2) {
-        numClasses = scanner.nextLine();
-    } else if (line % 3 == 0) {
-        studentNumber = scanner.nextLine();
-        
-        Students Student = new Students(name, numClasses, studentNumber);
-        //need to add objects to the multi-dimmensional array
-         AllStudents.add(Student);
-    } 
-    line++;
-}
-scanner.close();
-
-/*
-for (int row=0; row<all_students.length; row++) {
-            System.out.println(all_students[row]);
-            for (int col=0; col<all_students[row].length; col++) {
-                System.out.println(all_students[row][col]);
-                System.out.println(all_students[0][2]);
-            }
-        }
-
-
-      public String findStudentByName (String name, ArrayList<Students> AllStudents){
-        for (Students St : AllStudents){
-            if(St.getStudentName().equalsIgnoreCase(name)) return St;
-        }
-      return null;
-    }  
-    
-}
-*/
-
-
